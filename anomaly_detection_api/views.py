96a04c2363e6af1aab860a164bc60848c3145e91
meyi96 <fabioandresmejia@hotmail.com>
2021-12-07 01:35:46 -0500

    modify out json

--- anomaly_detection_api/views.py
+++ anomaly_detection_api/views.py
@@ -13,16 +13,13 @@ class checkAnomalyUmbral(View):
 
     def get(self, request, experiment_id):
         experiment_id =str(experiment_id)
-        result=[]
         responsedb=detectionByUmbralService.search_anomaly(detectionByUmbralService,experiment_id)
         if(len(responsedb)>0):
             protocol = Experiment.objects.get(pk=experiment_id).protocol
             header = {'experiment_id': experiment_id, 'protocol': protocol, 'detect_anomaly': True, 'detections_number': len(responsedb) }
-            result.append(header)
-            result.append(responsedb)
+            header['detections']= responsedb
         else:
            header = {'experiment_id': experiment_id, 'detect_anomaly': False, }
-           result.append(header) 
 
-        return JsonResponse(result, safe=False)
+        return JsonResponse(header, safe=False)
 
